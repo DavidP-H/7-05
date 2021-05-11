0ed9be340eb17bd3f9a31d296dd6f62d0d02e985
DavidHart05 <>
2021-05-11 13:33:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let day
+let age
+
+document.getElementById('button').addEventListener('click', checker)
+
+function checker () {
+  day = document.getElementById('input1').value
+  age = parseInt(document.getElementById('input2').value)
+  if (day === 'Saturday' || day === 'Sunday') {
+    document.getElementById('result').innerHTML = 'Time to relax for the weekend!'
+  } else if (age < 18) {
+    document.getElementById('result').innerHTML = 'Time for School!'
+  } else if (age > 18) {
+    document.getElementById('result').innerHTML = 'Time for Work!'
+  }
+}
