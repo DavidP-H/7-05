ee8e98ebd5d28a66680d14c2bf8e67ff0746bf4c
DavidHart05 <>
2021-05-11 13:41:45 +0000

    Fixed NOT signs on the extra document

--- withAndOr.js
+++ withAndOr.js
@@ -8,9 +8,9 @@ function checker () {
   age = parseInt(document.getElementById('input2').value)
   if (day === 'Saturday' || day === 'Sunday') {
     document.getElementById('result').innerHTML = 'Time to relax for the weekend!'
-  } else if ((day != 'Saturday' || day != 'Sunday') && (age < 18)) {
+  } else if ((day !== 'Saturday' || day !== 'Sunday') && (age < 18)) {
     document.getElementById('result').innerHTML = 'Time for School!'
-  } else if ((day != 'Saturday' || day != 'Sunday') && (age > 18)) {
+  } else if ((day !== 'Saturday' || day !== 'Sunday') && (age > 18)) {
     document.getElementById('result').innerHTML = 'Time for Work!'
   }
 }
